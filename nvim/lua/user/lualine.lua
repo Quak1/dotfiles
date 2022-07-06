8c225c443e8e03e70f34817f2f50f1ed73617189
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-06 16:11:46 -0700

    nvim - comment default settings

--- nvim/lua/user/lualine.lua
+++ nvim/lua/user/lualine.lua
@@ -1,18 +1,23 @@
 require('lualine').setup {
   options = {
-    icons_enabled = true,
-    theme = 'auto',
-    component_separators = { left = '', right = '' },
-    section_separators = { left = '', right = '' },
-    disabled_filetypes = {},
-    always_divide_middle = true,
-    globalstatus = false,
+    -- icons_enabled = true,
+    -- theme = 'auto',
+    -- component_separators = { left = '', right = '' },
+    -- section_separators = { left = '', right = '' },
+    -- disabled_filetypes = {},
+    -- always_divide_middle = true,
+    globalstatus = true,
   },
   -- buffer, windows, tabs, filesize, hostname
   sections = {
     lualine_a = { 'mode' },
     lualine_b = { 'branch', 'diff', 'diagnostics' },
-    lualine_c = { 'filename' },
+    lualine_c = {
+      {
+        'filename',
+        path = 1,
+      },
+    },
     lualine_x = { 'encoding', 'fileformat', 'filetype' },
     lualine_y = { 'progress' },
     lualine_z = { 'location' }
@@ -25,6 +30,6 @@ require('lualine').setup {
     lualine_y = {},
     lualine_z = {}
   },
-  tabline = {},
-  extensions = {}
+  -- tabline = {},
+  -- extensions = {"nvim-tree"}
 }
