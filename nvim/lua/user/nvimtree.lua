50f7aa554246b51521f4d4a3d4c0dac96e21a392
Aldo Camacho <aldo.camacho98@gmail.com>
2022-07-18 14:38:56 -0700

    nvim config

--- nvim/lua/user/nvimtree.lua
+++ nvim/lua/user/nvimtree.lua
@@ -20,11 +20,11 @@ require("nvim-tree").setup {
 }
 -- Auto close when last window open
 -- https://github.com/kyazdani42/nvim-tree.lua/discussions/1115
-vim.api.nvim_create_autocmd("BufEnter", {
+--[[ vim.api.nvim_create_autocmd("BufEnter", {
   nested = true,
   callback = function()
     if #vim.api.nvim_list_wins() == 1 and vim.api.nvim_buf_get_name(0):match("NvimTree_") ~= nil then
       vim.cmd "quit"
     end
   end
-})
+}) ]]
