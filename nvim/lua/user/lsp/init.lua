8603ffb50143c23d8490d96a1e9bfeba8ab9eee8
Aldo Camacho <aldo.camacho98@gmail.com>
2022-08-05 10:25:21 -0700

    neovim install mason.nvim

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,16 +1,13 @@
-local lspconfig = require "lspconfig"
-require("nvim-lsp-installer").setup {}
+local lspconfig = require("lspconfig")
+require("nvim-lsp-installer").setup({})
+require("user.lsp.mason")
 local handlers = require("user.lsp.handlers")
 handlers.setup()
 
-lspconfig.util.default_config = vim.tbl_extend(
-  "force",
-  lspconfig.util.default_config,
-  {
-    --capabilities = handlers.capabilities,
-    on_attach = handlers.on_attach
-  }
-)
+lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
+	--capabilities = handlers.capabilities,
+	on_attach = handlers.on_attach,
+})
 
 lspconfig["sumneko_lua"].setup(require("user.lsp.settings.sumneko_lua"))
 lspconfig["tsserver"].setup({})
