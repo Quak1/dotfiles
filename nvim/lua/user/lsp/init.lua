f20b7ddb3f66557de66bcf41431a411ded47ffea
Aldo Camacho <aldo.camacho98@gmail.com>
2022-08-05 11:34:32 -0700

    neovim update lsp config

--- nvim/lua/user/lsp/init.lua
+++ nvim/lua/user/lsp/init.lua
@@ -1,13 +1,18 @@
-local lspconfig = require("lspconfig")
-require("nvim-lsp-installer").setup({})
 require("user.lsp.mason")
+local lspconfig = require("lspconfig")
 local handlers = require("user.lsp.handlers")
 handlers.setup()
 
-lspconfig.util.default_config = vim.tbl_extend("force", lspconfig.util.default_config, {
-	--capabilities = handlers.capabilities,
-	on_attach = handlers.on_attach,
-})
+-- The global defaults for all servers can be overridden by extending the `default_config` table
+lspconfig.util.default_config = vim.tbl_extend(
+  "force",
+  lspconfig.util.default_config,
+  {
+    --capabilities = handlers.capabilities,
+    on_attach = handlers.on_attach,
+  }
+)
 
+-- Server specific configuration
 lspconfig["sumneko_lua"].setup(require("user.lsp.settings.sumneko_lua"))
 lspconfig["tsserver"].setup({})
--- nvim/lua/user/plugins.lua
+++ nvim/lua/user/plugins.lua
@@ -65,7 +65,6 @@ return packer.startup(function(use)
 
   -- LSP
   use "neovim/nvim-lspconfig" -- enable LSP
-  use "williamboman/nvim-lsp-installer" -- simple to use language server installer
   use "williamboman/mason.nvim" -- manage and install LSP servers, linters, and more
   use "williamboman/mason-lspconfig.nvim" -- bridges mason.nvim with lspconfig
   use "jose-elias-alvarez/null-ls.nvim" -- provides formatting and diagnostics
