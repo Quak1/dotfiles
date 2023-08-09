cff65e83946f08973dc2d28a4a3437ee31e2cb1e
Aldo Camacho <aldo@aldocamacho.com>
2023-08-09 16:41:49 -0700

    formatting

--- nvim/lua/plugins/init.lua
+++ nvim/lua/plugins/init.lua
@@ -7,18 +7,6 @@ return {
 	--"lewis6991/impatient.nvim",
 	--"lukas-reineke/indent-blankline.nvim",
 
-	-- Colorschemes
-	"navarasu/onedark.nvim",
-	{
-		"bluz71/vim-nightfly-colors",
-		name = "nightfly",
-		lazy = false,
-		priority = 1000,
-		config = function()
-			vim.cmd.colorscheme("nightfly")
-		end,
-	},
-
 	-- Telescope
 	{
 		"nvim-telescope/telescope.nvim",
@@ -26,18 +14,6 @@ return {
 		dependencies = { { "nvim-lua/plenary.nvim" } },
 	},
 	--"ahmedkhalf/project.nvim", -- has telescope integration
-
-	-- Treesitter
-	{ "nvim-treesitter/nvim-treesitter", build = ":TSUpdate" },
-
-	-- Greeting window
-	{
-		"goolord/alpha-nvim",
-		dependencies = { "nvim-tree/nvim-web-devicons" },
-		config = function()
-			require("alpha").setup(require("alpha.themes.startify").config)
-		end,
-	},
 
 	-- Comment
 	--"numToStr/Comment.nvim",
