a3d5b9f6243c846f67e12d9dee58835e235ee9d9
Aldo Camacho <me@aldocamacho.com>
2025-03-08 22:29:11 -0800

    remove lsp-zero from nvim lsp config

--- nvim/lua/plugins/lsp.lua
+++ nvim/lua/plugins/lsp.lua
@@ -1,15 +1,22 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+	desc = "LSP actions",
+	callback = function(event)
+		local opts = { buffer = event.buf }
+
+		vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+		vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+		vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+		vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+		vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+		vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+		vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+		vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+		vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+		vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+	end,
+})
+
 return {
-	{
-		"VonHeikemen/lsp-zero.nvim",
-		branch = "v4.x",
-		lazy = true,
-		config = false,
-		init = function()
-			-- Disable automatic setup, we are doing it manually
-			vim.g.lsp_zero_extend_cmp = 0
-			vim.g.lsp_zero_extend_lspconfig = 0
-		end,
-	},
 	{
 		"williamboman/mason.nvim",
 		lazy = false,
@@ -26,30 +33,12 @@ return {
 			{ "saadparwaiz1/cmp_luasnip" },
 		},
 		config = function()
-			-- Here is where you configure the autocompletion settings.
-			local lsp_zero = require("lsp-zero")
-			lsp_zero.extend_cmp()
-
-			-- And you can configure cmp even more, if you want to.
 			local cmp = require("cmp")
-			local cmp_action = lsp_zero.cmp_action()
 
 			-- friendly-snippets
 			require("luasnip.loaders.from_vscode").lazy_load()
 
 			cmp.setup({
-				formatting = lsp_zero.cmp_format({ details = true }),
-				mapping = cmp.mapping.preset.insert({
-					["<C-Space>"] = cmp.mapping.complete(),
-					["<C-u>"] = cmp.mapping.scroll_docs(-4),
-					["<C-d>"] = cmp.mapping.scroll_docs(4),
-					["<C-f>"] = cmp_action.luasnip_jump_forward(),
-					["<C-b>"] = cmp_action.luasnip_jump_backward(),
-
-					["<CR>"] = cmp.mapping.confirm({ select = true }),
-					["<Tab>"] = cmp_action.luasnip_supertab(),
-					["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-				}),
 				snippet = {
 					expand = function(args)
 						require("luasnip").lsp_expand(args.body)
@@ -59,6 +48,63 @@ return {
 					{ name = "nvim_lsp" },
 					{ name = "luasnip" },
 				},
+				mapping = cmp.mapping.preset.insert({
+					["<C-Space>"] = cmp.mapping.complete(),
+					["<C-u>"] = cmp.mapping.scroll_docs(-4),
+					["<C-d>"] = cmp.mapping.scroll_docs(4),
+
+					-- Jump to the next snippet placeholder
+					["<C-f>"] = cmp.mapping(function(fallback)
+						local luasnip = require("luasnip")
+						if luasnip.locally_jumpable(1) then
+							luasnip.jump(1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					-- Jump to the previous snippet placeholder
+					["<C-b>"] = cmp.mapping(function(fallback)
+						local luasnip = require("luasnip")
+						if luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+
+					-- Use Enter to confirm completion
+					["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+					-- Super tab
+					["<Tab>"] = cmp.mapping(function(fallback)
+						local luasnip = require("luasnip")
+						local col = vim.fn.col(".") - 1
+
+						if cmp.visible() then
+							cmp.select_next_item({ behavior = "select" })
+						elseif luasnip.expand_or_locally_jumpable() then
+							luasnip.expand_or_jump()
+						elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+							fallback()
+						else
+							cmp.complete()
+						end
+					end, { "i", "s" }),
+
+					-- Super shift tab
+					["<S-Tab>"] = cmp.mapping(function(fallback)
+						local luasnip = require("luasnip")
+
+						if cmp.visible() then
+							cmp.select_prev_item({ behavior = "select" })
+						elseif luasnip.locally_jumpable(-1) then
+							luasnip.jump(-1)
+						else
+							fallback()
+						end
+					end, { "i", "s" }),
+				}),
 			})
 		end,
 	},
@@ -73,18 +119,14 @@ return {
 			{ "williamboman/mason-lspconfig.nvim" },
 		},
 		config = function()
-			local lsp_zero = require("lsp-zero")
-
-			local lsp_attach = function(client, bufnr)
-				-- see :help lsp-zero-keybindings to learn the available actions
-				lsp_zero.default_keymaps({ buffer = bufnr })
-			end
-
-			lsp_zero.extend_lspconfig({
-				capabilities = require("cmp_nvim_lsp").default_capabilities(),
-				lsp_attach = lsp_attach,
-				sign_text = true,
-			})
+			-- Add cmp_nvim_lsp capabilities settings to lspconfig
+			-- This should be executed before you configure any language server
+			local lspconfig_defaults = require("lspconfig").util.default_config
+			lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+				"force",
+				lspconfig_defaults.capabilities,
+				require("cmp_nvim_lsp").default_capabilities()
+			)
 
 			require("mason-lspconfig").setup({
 				ensure_installed = { "ts_ls", "lua_ls", "html", "cssls" },
@@ -98,11 +140,6 @@ return {
 						require("lspconfig")[server_name].setup({})
 					end,
 
-					-- "custom handler" for `lua_ls`
-					lua_ls = function()
-						local lua_opts = lsp_zero.nvim_lua_ls()
-						require("lspconfig").lua_ls.setup(lua_opts)
-					end,
 					-- setup html to honor templating tags while formatting
 					-- https://code.visualstudio.com/docs/languages/html#_formatting
 					html = function()
@@ -159,6 +196,7 @@ return {
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				javascriptreact = { "prettierd" },
 				pug = { "prettierd" },
+				json = { "prettierd" },
 			},
 			-- Set up format-on-save
 			format_on_save = {
