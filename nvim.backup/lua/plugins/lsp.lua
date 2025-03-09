3d3c549f824c966df57a28f92901134eef659541
Aldo Camacho <me@aldocamacho.com>
2025-03-09 14:34:53 -0700

    install nvchad for nvim configuration

---
+++ nvim.backup/lua/plugins/lsp.lua
@@ -0,0 +1,210 @@
+vim.api.nvim_create_autocmd("LspAttach", {
+    desc = "LSP actions",
+    callback = function(event)
+        local opts = { buffer = event.buf }
+
+        vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover()<cr>", opts)
+        vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<cr>", opts)
+        vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<cr>", opts)
+        vim.keymap.set("n", "gi", "<cmd>lua vim.lsp.buf.implementation()<cr>", opts)
+        vim.keymap.set("n", "go", "<cmd>lua vim.lsp.buf.type_definition()<cr>", opts)
+        vim.keymap.set("n", "gr", "<cmd>lua vim.lsp.buf.references()<cr>", opts)
+        vim.keymap.set("n", "gs", "<cmd>lua vim.lsp.buf.signature_help()<cr>", opts)
+        vim.keymap.set("n", "<F2>", "<cmd>lua vim.lsp.buf.rename()<cr>", opts)
+        vim.keymap.set({ "n", "x" }, "<F3>", "<cmd>lua vim.lsp.buf.format({async = true})<cr>", opts)
+        vim.keymap.set("n", "<F4>", "<cmd>lua vim.lsp.buf.code_action()<cr>", opts)
+    end,
+})
+
+return {
+    -- LSP
+    {
+        "williamboman/mason.nvim",
+        lazy = false,
+        config = true
+    },
+    {
+        "williamboman/mason-lspconfig.nvim",
+        opts = {
+            ensure_installed = { "lua_ls", "html", "cssls", "clangd", },
+            handlers = {
+                -- this first function is the "default handler"
+                -- applies to every language server without a "custom handler"
+                function(server_name)
+                    require("lspconfig")[server_name].setup({})
+                end,
+
+                -- setup html to honor templating tags while formatting
+                -- https://code.visualstudio.com/docs/languages/html#_formatting
+                html = function()
+                    require("lspconfig").html.setup({
+                        settings = {
+                            html = {
+                                format = {
+                                    templating = true,
+                                    wrapLineLength = 1000,
+                                    -- wrapAttributes = "force-aligned",
+                                    wrapAttributes = "auto",
+                                },
+                            },
+                        },
+                    })
+                end,
+            },
+        }
+    },
+    {
+        "neovim/nvim-lspconfig",
+        cmd = { "LspInfo", "LspInstall", "LspStart" },
+        event = { "BufReadPre", "BufNewFile" },
+        dependencies = {
+            "mason.nvim",
+            "mason-lspconfig.nvim",
+            { "hrsh7th/cmp-nvim-lsp" },
+        },
+        config = function()
+            -- Add cmp_nvim_lsp capabilities settings to lspconfig
+            local lspconfig_defaults = require("lspconfig").util.default_config
+            lspconfig_defaults.capabilities = vim.tbl_deep_extend(
+                "force",
+                lspconfig_defaults.capabilities,
+                require("cmp_nvim_lsp").default_capabilities()
+            )
+        end,
+    },
+    {
+        "pmizio/typescript-tools.nvim",
+        dependencies = { "nvim-lua/plenary.nvim", "neovim/nvim-lspconfig" },
+        opts = {
+            settings = {
+                tsserver_plugins = {
+                    "@styled/typescript-styled-plugin",
+                },
+            },
+        },
+    },
+
+    -- Autocompletion
+    {
+        "hrsh7th/nvim-cmp",
+        event = "InsertEnter",
+        dependencies = {
+            { "L3MON4D3/LuaSnip" },
+            { "rafamadriz/friendly-snippets" },
+            { "hrsh7th/nvim-cmp" },
+            { "saadparwaiz1/cmp_luasnip" },
+        },
+        config = function()
+            local cmp = require("cmp")
+
+            -- friendly-snippets
+            require("luasnip.loaders.from_vscode").lazy_load()
+
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                sources = {
+                    { name = "nvim_lsp" },
+                    { name = "luasnip" },
+                    { name = "buffer" },
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ["<C-Space>"] = cmp.mapping.complete(),
+                    ["<C-u>"] = cmp.mapping.scroll_docs(-4),
+                    ["<C-d>"] = cmp.mapping.scroll_docs(4),
+
+                    -- Jump to the next snippet placeholder
+                    ["<C-f>"] = cmp.mapping(function(fallback)
+                        local luasnip = require("luasnip")
+                        if luasnip.locally_jumpable(1) then
+                            luasnip.jump(1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    -- Jump to the previous snippet placeholder
+                    ["<C-b>"] = cmp.mapping(function(fallback)
+                        local luasnip = require("luasnip")
+                        if luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+
+                    -- Use Enter to confirm completion
+                    ["<CR>"] = cmp.mapping.confirm({ select = false }),
+
+                    -- Super tab
+                    ["<Tab>"] = cmp.mapping(function(fallback)
+                        local luasnip = require("luasnip")
+                        local col = vim.fn.col(".") - 1
+
+                        if cmp.visible() then
+                            cmp.select_next_item({ behavior = "select" })
+                        elseif luasnip.expand_or_locally_jumpable() then
+                            luasnip.expand_or_jump()
+                        elseif col == 0 or vim.fn.getline("."):sub(col, col):match("%s") then
+                            fallback()
+                        else
+                            cmp.complete()
+                        end
+                    end, { "i", "s" }),
+
+                    -- Super shift tab
+                    ["<S-Tab>"] = cmp.mapping(function(fallback)
+                        local luasnip = require("luasnip")
+
+                        if cmp.visible() then
+                            cmp.select_prev_item({ behavior = "select" })
+                        elseif luasnip.locally_jumpable(-1) then
+                            luasnip.jump(-1)
+                        else
+                            fallback()
+                        end
+                    end, { "i", "s" }),
+                }),
+            })
+        end,
+    },
+
+    -- Formatter
+    {
+        "stevearc/conform.nvim",
+        event = { "BufWritePre" },
+        cmd = { "ConformInfo" },
+        keys = {
+            {
+                -- Customize or remove this keymap to your liking
+                "<leader>F",
+                function()
+                    require("conform").format({ async = true, lsp_format = "fallback" })
+                end,
+                mode = "",
+                desc = "Format buffer",
+            },
+        },
+        -- Everything in opts will be passed to setup()
+        opts = {
+            -- Define your formatters
+            formatters_by_ft = {
+                lua = { "stylua" },
+                javascript = { "prettierd", "prettier", stop_after_first = true },
+                javascriptreact = { "prettierd" },
+                pug = { "prettierd" },
+                json = { "prettierd" },
+            },
+            -- Set up format-on-save
+            format_on_save = {
+                timeout_ms = 500,
+                lsp_format = "fallback",
+            },
+            -- Customize formatters
+            formatters = {},
+        },
+    },
+}
