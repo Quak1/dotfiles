fae6922e8d6fd51d38913833178a58fd0e63d3a1
Aldo Camacho <me@aldocamacho.com>
2025-08-30 13:35:21 -0700

    nvim add autocompletion tab support

---
+++ nvim/lua/plugins/blink.cmp.lua
@@ -0,0 +1,15 @@
+return {
+  "Saghen/blink.cmp",
+  opts = {
+    completion = {
+      list = {
+        selection = { preselect = false, auto_insert = true },
+      },
+    },
+    keymap = {
+      preset = "enter",
+      ["<Tab>"] = { "snippet_forward", "select_next", "fallback" },
+      ["<S-Tab>"] = { "snippet_backward", "select_prev", "fallback" },
+    },
+  },
+}
