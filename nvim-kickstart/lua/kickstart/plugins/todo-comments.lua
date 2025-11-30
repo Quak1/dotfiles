02cd62a2902207912c4950ed6fc832055aee3558
Aldo Camacho <me@aldocamacho.com>
2025-11-30 02:40:33 -0800

    disable todo-comments plugin

--- nvim-kickstart/lua/kickstart/plugins/todo-comments.lua
+++ nvim-kickstart/lua/kickstart/plugins/todo-comments.lua
@@ -1,6 +1,7 @@
 return {
   -- Highlight todo, notes, etc in comments
   {
+    enabled = false,
     'folke/todo-comments.nvim',
     event = 'VimEnter',
     dependencies = { 'nvim-lua/plenary.nvim' },
