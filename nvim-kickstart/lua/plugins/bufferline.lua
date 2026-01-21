08319b59ce134b331bff121d99b477f6b8e82692
Aldo Camacho <me@aldocamacho.com>
2026-01-21 14:06:09 -0800

    add plugin bufferline.nvim

---
+++ nvim-kickstart/lua/plugins/bufferline.lua
@@ -0,0 +1,49 @@
+return {
+  'akinsho/bufferline.nvim',
+  event = 'VeryLazy',
+  keys = {
+    { '<leader>bp', '<Cmd>BufferLineTogglePin<CR>', desc = 'Toggle Pin' },
+    { '<leader>bP', '<Cmd>BufferLineGroupClose ungrouped<CR>', desc = 'Delete Non-Pinned Buffers' },
+    { '<leader>br', '<Cmd>BufferLineCloseRight<CR>', desc = 'Delete Buffers to the Right' },
+    { '<leader>bl', '<Cmd>BufferLineCloseLeft<CR>', desc = 'Delete Buffers to the Left' },
+    { '<S-h>', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { '<S-l>', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '[b', '<cmd>BufferLineCyclePrev<cr>', desc = 'Prev Buffer' },
+    { ']b', '<cmd>BufferLineCycleNext<cr>', desc = 'Next Buffer' },
+    { '[B', '<cmd>BufferLineMovePrev<cr>', desc = 'Move buffer prev' },
+    { ']B', '<cmd>BufferLineMoveNext<cr>', desc = 'Move buffer next' },
+  },
+  opts = {
+    options = {
+      diagnostics = 'nvim_lsp',
+      diagnostics_indicator = function(count, level, diagnostics_dict, context)
+        local icon = level:match 'error' and ' ' or ' '
+        return ' ' .. icon .. count
+      end,
+      offsets = {
+        {
+          filetype = 'neo-tree',
+          text = 'Neo-tree',
+          highlight = 'Directory',
+          text_align = 'left',
+        },
+        {
+          filetype = 'snacks_layout_box',
+        },
+      },
+      show_buffer_close_icons = false,
+      separator_style = 'slant',
+    },
+  },
+  config = function(_, opts)
+    require('bufferline').setup(opts)
+    -- Fix bufferline when restoring a session
+    vim.api.nvim_create_autocmd({ 'BufAdd', 'BufDelete' }, {
+      callback = function()
+        vim.schedule(function()
+          pcall(nvim_bufferline)
+        end)
+      end,
+    })
+  end,
+}
