75b2d49317716632dae61874dd4efebb7db50f90
Aldo Camacho <me@aldocamacho.com>
2026-03-18 15:36:26 -0700

    copy configuration from old dotfiles

---
+++ nvim/.config/nvim/lua/plugins/project.lua
@@ -0,0 +1,9 @@
+return { -- Automatically set cwd to project root directory
+  'DrKJeff16/project.nvim',
+  -- dependencies = { -- OPTIONAL
+  --   'nvim-lua/plenary.nvim',
+  --   -- 'nvim-telescope/telescope.nvim',
+  --   'ibhagwan/fzf-lua',
+  -- },
+  opts = {},
+}
